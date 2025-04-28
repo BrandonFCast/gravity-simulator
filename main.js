565f2ad83bf46f4f9d0edb341f72b0fb3c135711
Brandon BC <brand.bc0@gmail.com>
2025-04-27 21:24:09 -0600

    added example

--- main.js
+++ main.js
@@ -8,25 +8,68 @@ const ctx = cnv.getContext('2d');
 cnv.width = window.innerWidth;
 cnv.height = window.innerHeight;
 
-const config = new Config({ ctx });
+const config = new Config({ ctx: ctx, gravityForce: 1 });
 Config.ctx = ctx;
 Entity.setConfig(config);
 
 
-const particle = new Particle({
-    x: 500,
-    y: 300
+const blue = new Particle({
+    x: 1400,
+    y: 1000,
+    color: "cyan",
+    mass: 1,
+    debugMode: true,
+    debugColor: 'green'
+});
+const orange = new Particle({
+    x: 1500,
+    y: 900,
+    color: "orange",
+    mass: 200,
+    debugMode: true,
+    debugColor: "red"
 });
 
+const start = () => {
+    setInterval(() => {
+        ctx.clearRect(0, 0, cnv.width, cnv.height);
+        Entity.calculateTotalForces();
+        for (const entity of Entity.entities) {
+            entity.update();
+        }
+    }, 1);
 
-setInterval(() => {
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
-    for (const entity of Entity.entities) {
-        entity.update();
+    const f = 1;
+    blue.addForce({ angle: 0, force: f })
+        // orange.addForce({ angle: 0, force: 40 })
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "a") {
+        start();
     }
-}, 100);
+})
 
-particle.addForce({ angle: 180, force: 10 })
-setTimeout(() => {
-    particle.addForce({ angle: 270, force: 10 })
-}, 2000)
+cnv.addEventListener("click", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    const mass = prompt("Enter the mass of the particle");
+    const angle = prompt("Enter the angle of force");
+    const force = prompt("Enter the force of the particle");
+
+    const particle = new Particle({
+        x,
+        y,
+        color: "red",
+        mass: mass
+    })
+    particle.addForce({ angle: angle, force: force })
+    for (const entity of Entity.entities) {
+        entity.draw(Config.ctx);
+    }
+
+})
+
+for (const entity of Entity.entities) {
+    entity.draw(Config.ctx);
+}
